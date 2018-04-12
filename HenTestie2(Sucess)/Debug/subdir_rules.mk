61d99bcf28b3e56ab6d6e959306aa3d6f222e3d6
sad <Noirpale@gmail.com>
2018-04-12 13:59:20 +0200

    Revert "KnEP diG"
    This reverts commit 79a957c1fc5879e0b341e0ad486408efd3f743cf.

--- HenTestie2(Sucess)/Debug/subdir_rules.mk
+++ HenTestie2(Sucess)/Debug/subdir_rules.mk
@@ -6,7 +6,7 @@
 %.o: ../%.c $(GEN_OPTS) | $(GEN_HDRS)
 	@echo 'Building file: "$<"'
 	@echo 'Invoking: GNU Compiler'
-	"/home/sad/Desktop/4.sem/EMP/Tools/CCS7/ccsv7/tools/compiler/gcc-arm-none-eabi-6-2017-q1-update/bin/arm-none-eabi-gcc" -c -mcpu=cortex-m4 -march=armv7e-m -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -DPART_TM4C123GH6PM -I"/home/sad/gitrepos/HerpDerpStuff/HenTestie2(Sucess)" -I"/home/sad/Desktop/4.sem/EMP/Tools/CCS7/ccsv7/tools/compiler/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include" -ffunction-sections -fdata-sections -g -gdwarf-3 -gstrict-dwarf -Wall -specs="nosys.specs" -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" $(GEN_OPTS__FLAG) -o"$@" "$(shell echo $<)"
+	"/home/sad/Desktop/4.sem/EMP/Tools/CCS7/ccsv7/tools/compiler/gcc-arm-none-eabi-6-2017-q1-update/bin/arm-none-eabi-gcc" -c -mcpu=cortex-m4 -march=armv7e-m -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -DPART_TM4C123GH6PM -I"/home/sad/CCS/HenTestie2(Sucess)" -I"/home/sad/Desktop/4.sem/EMP/Tools/CCS7/ccsv7/tools/compiler/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include" -ffunction-sections -fdata-sections -g -gdwarf-3 -gstrict-dwarf -Wall -specs="nosys.specs" -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" $(GEN_OPTS__FLAG) -o"$@" "$(shell echo $<)"
 	@echo 'Finished building: "$<"'
 	@echo ' '
 
